81ce982bf1c3145954cdd472ca0a97f6cbdd0398
moyshik7 <moyshik7@gmail.com>
2023-04-20 00:59:52 +0600

    Test 1681930792580270800

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 for i in {1..5}
 do
-    echo "this is a line $(date +%s)" > file.txt
+    echo "this is a line $(date +%s%N)" > file.txt
     git add .
-    git commit -m "Test $(date +%s)"
+    git commit -m "Test $(date +%s%N)"
 done
